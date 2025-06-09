7d6036cc06fd1fc9e1e386e5ac8d187d30e716d2
TheNT 87 <nt@eigh.tk>
2025-06-09 18:36:32 +0200

    switch to liestar

--- api/index.py
+++ api/index.py
@@ -1,8 +1,14 @@
-from sanic import Sanic
-from sanic.response import text
+from litestar import Litestar, get
 
-app = Sanic("HorpionAPI")
 
-@app.get("/")
-async def hello_world(request):
-    return text("Hello, world.")
+@get("/")
+async def index() -> str:
+    return "Hello, world!"
+
+
+@get("/books/{book_id:int}")
+async def get_book(book_id: int) -> dict[str, int]:
+    return {"book_id": book_id}
+
+
+app = Litestar([index, get_book])
